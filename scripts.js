c31dfe04efcabab3a6b55ab57bc2eea034dbf76a
Mészáros László <laszlo.meszaros.connect@gmail.com>
2025-02-05 17:53:50 +0100

    [GL-1] Fromat script.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-document.getElementById("rollDiceBtn").addEventListener("click",function () {
-   const diceResult = Math.floor(Math.random*6)+1; // 1 és 6 közötti egész szám
-   document.getElementById(diceResult).innerHTML = `🎲${diceResult}` ;
+document.getElementById("rollDiceBtn").addEventListener("click", function () {
+    const diceResult = Math.floor(Math.random * 6) + 1; // 1 és 6 közötti egész szám
+    document.getElementById(diceResult).innerHTML = `🎲${diceResult}`;
 });
