1ebed1facc3e6742a5dc22942e04390609f58d95
Mészáros László <laszlo.meszaros.connect@gmail.com>
2025-02-05 17:55:25 +0100

    [GL-1] Fix click function in script.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 document.getElementById("rollDiceBtn").addEventListener("click", function () {
-    const diceResult = Math.floor(Math.random * 6) + 1; // 1 és 6 közötti egész szám
-    document.getElementById(diceResult).innerHTML = `🎲${diceResult}`;
+    const diceResult = Math.floor(Math.random() * 6) + 1; // 1 és 6 közötti egész szám
+    document.getElementById("diceResult").innerHTML = `🎲${diceResult}`;
 });
